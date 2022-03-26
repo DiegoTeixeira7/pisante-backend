cadb96a0255e3ac97de39a6ff2d027441812a2b7
DiegoTeixeira7 <dieg07teixeira@outlook.com>
2022-03-26 09:59:49 -0300

    habilitado cors

--- public_html/index.php
+++ public_html/index.php
@@ -1,5 +1,8 @@
 <?php
     header('Content-Type: application/json');
+    header("Access-Control-Allow-Origin: *");
+    header('Access-Control-Allow-Credentials: true');
+    header('Access-Control-Max-Age: 86400');    // cache for 1 day
 
     require_once '../vendor/autoload.php';
 
